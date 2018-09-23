42838be1dfe9e8ed510f4eac0530d101c5dd3e06
thedocbwarren <jedi.master@augmentedjs.com>
2018-09-23 15:29:18 -0700

    add noTime to formatDate

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,9 +1,10 @@
 /**
- * Format a date to US format (MM/DD/YYYY HH:mm AM/PM)
+ * Format a date to US format (MM/DD/YYYY HH:mm AM/PM) or (MM/DD/YYYY) only
  * @param {date} date The date to format
+ * @param {boolean} noTime Exclude the time
  * @returns {string} The formatted date
  */
-const formatDate = (date) => {
+const formatDate = (date, noTime) => {
   const year = date.getFullYear(),
     month = date.getMonth() + 1, // months are zero indexed
     day = date.getDate(),
@@ -13,7 +14,9 @@ const formatDate = (date) => {
     hourFormatted = hour % 12 || 12, // hour returned in 24 hour format
     minuteFormatted = minute < 10 ? "0" + minute : minute,
     morning = hour < 12 ? "am" : "pm";
-
+  if (noTime) {
+    return `${month}/${day}/${year}`;
+  }
   return `${month}/${day}/${year} ${hourFormatted}:${minuteFormatted}${morning}`;
 };
 
